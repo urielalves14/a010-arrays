d36ed5a890f38369818108de61ce4cb3f9b9e310
urielalves14 <alvesuriel57@gmail.com>
2022-09-01 21:03:23 -0300

    atividadearray

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica
+const numero = [2,5,98,89,43,667,900]
+console.log(numero)
+// - O segundo array deve possuir apenas **strings**
+const string = ["uriel", "marco", "elton"]
+console.log(string)
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const mista = [4, "fany", true]
+console.log(mista)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numero = [2,5,98,89,43,667,900]
+const string = ["uriel", "marco", "elton"]
+const mista = [4, "fany", true]
+
+console.log(numero.length)
+console.log(string.length)
+console.log(mista.length)
+
+// - Imprima o primeiro item do primeiro array, o segundo item do segundo array, e o terceiro item do terceiro array
+console.log(`${numero[0]}, ${string[1]}, ${mista[2]} `)
+
+// - Imprima uma informação booleana verdadeira sobre a **inclusão** de um item do primeiro array, e uma informação booleana falsa sobre a **inclusão** de um item ao terceiro array. Isto é, um `includes()` verdadeiro, e outro falso.
+console.log(numero.includes(2))
+console.log(mista.includes("agnes"))
